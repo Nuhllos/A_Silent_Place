10740c8ea3fe92a94828f8b92f0ce5103d78a989
Rafael <rafaelg.aires@gmail.com>
2021-08-09 13:53:38 -0300

    Update signin.php

--- site/php/signin.php
+++ site/php/signin.php
@@ -12,16 +12,20 @@ if (empty($_POST["user"]) || empty($_POST["password"])) {
 $user = mysqli_real_escape_string($connection, $_POST["user"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 
-$query = "select user_id, user from users where user = '{$user}' and password = md5('$password')";
+$query = "select user_id, user, user_status from users where user = '{$user}' and password = md5('$password')";
 
 $result = mysqli_query($connection, $query);
+
+$row0 = mysqli_fetch_assoc($result);
+
+$u_s = $row0["user_status"];
 
 $row = mysqli_num_rows($result);
 
 if ($row == 1) {
     $_SESSION["user"] = $user;
     $_SESSION["authenticated"] = true;
-    if ($_SESSION["user_status"] == 0) {
+    if ($u_s == 0) {
         $_SESSION["admin"] = true;
     }
     header("Location: ../../index.php#start");

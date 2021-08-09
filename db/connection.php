a9397d1045f2be36af97a885a04241d791ff0134
Rafael <rafaelg.aires@gmail.com>
2021-08-09 10:49:55 -0300

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
 
-define("HOST", "sql10.freesqldatabase.com");
-define("USER", "sql10429564");
-define("PASSWORD", "l29axlIYrE");
-define("DB", "sql10429564");
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DB", "silent_db");
 
 $connection = mysqli_connect(HOST, USER, PASSWORD, DB) or die ("Não foi possivel estabelecer uma conexão");
 ?>
